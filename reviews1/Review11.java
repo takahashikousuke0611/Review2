a397caa56ddfb9b2f180fee3d9256f36a4f77f94
takahashikousuke <241006@std.hi-joho.ac.jp>
2025-05-26 11:14:49 +0900

    Review

---
+++ reviews1/Review11.java
@@ -0,0 +1,9 @@
+package reviews.reviews1;
+
+public class Review11 {
+    public static void main(String[] args) {
+        int num = 2;
+        if(num % 10 >=1){
+        System.out.println()
+    }
+}
